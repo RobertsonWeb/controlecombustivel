7fd85f7beafdb900721629d50248b16b1d576e69
Robertson Ebling dos Santos <robertson@ersistemas.info>
2018-08-04 11:07:55 -0300

    Add Heroku configs

--- projeto/settings.py
+++ projeto/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 from decouple import config
 from dj_database_url import parse as db_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
